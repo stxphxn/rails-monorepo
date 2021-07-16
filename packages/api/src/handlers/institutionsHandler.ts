a63bf6c4c41d7bba3b676ce9e83d99d124725b2f
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2021-07-16 13:15:37 +0100

    API: add response to institutions handler

--- packages/api/src/handlers/institutionsHandler.ts
+++ packages/api/src/handlers/institutionsHandler.ts
@@ -1,11 +1,21 @@
+import authCall from "../utils/authCall";
+
 const institutionsHandler = async (request: Request): Promise<Response> => {
   if (request.method !== "POST") {
     return new Response("Method Not Allowed", {
       status: 405
-    })
+    });
   }
 
-  return new Response(`request method: ${request.method}`)
-}
+  const data = await authCall('https://api.yapily.com/institutions');
+  const json = JSON.stringify(data);
+
+  return new Response(json,
+    {
+      headers: {
+        "content-type": "application/json"
+      }
+    });
+};
 
 export default institutionsHandler;
