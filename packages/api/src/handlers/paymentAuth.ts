30a2d388a4123a0a90f11984e886b1016ac44019
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2021-07-22 15:48:25 +0100

    API: add paymentRequest type

--- packages/api/src/handlers/paymentAuth.ts
+++ packages/api/src/handlers/paymentAuth.ts
@@ -1,5 +1,90 @@
 import authCall from "../utils/authCall";
 
+
+type paymentRequest = {
+  amount: {
+    amount: number,
+    currency: string,
+  }, 
+  contextType?: string,
+  internationalPayment?: {
+    chargeBearer?: string,
+    currencyOfTransfer: string,
+    exchangeRateInformation?: {
+      foreignExchangeContractReference?: string,
+      rate?: number,
+      rateType: string,
+      unitCurrency: string,  
+    },
+    priority: string,
+    purpose?: string,
+  },
+  payee: {
+    accountIdentifications: {
+      identification: string,
+      type: string,
+    },
+    address?: {
+      addressLines? : string,
+      addressType? : string,
+      buildingNumber? :string,
+      country?: string,
+      county?: string,
+      department?:string,
+      postCode?: string,
+      streetName?: string,
+      subDepartment?: string,
+      townName?: string,
+    },
+    merchantCategoryCode?: string,
+    merchantId?: string,
+    name: string,
+  },
+  payer?: {
+    accountIdentifications: {
+      identification: string,
+      type: string,
+    },
+    address?: {
+      addressLines? : string,
+      addressType? : string,
+      buildingNumber? :string,
+      country?: string,
+      county?: string,
+      department?:string,
+      postCode?: string,
+      streetName?: string,
+      subDepartment?: string,
+      townName?: string,
+    },
+    name: string,
+  },
+  paymentDateTime?: string,
+  paymentIdempotencyId: string,
+  periodicPayment?: {
+    finalPaymentAmount: {
+      amount: number,
+      currency: string,
+    },
+    finalPaymentDateTime?: string,
+    frequency: {
+      executionDay?: number,
+      intervalMonth?: number,
+      intervalWeek?: number,
+      type: string,
+    },
+    nextPaymentAmount: {
+      amount: number,
+      currency: string,
+    },
+    nextPaymentDateTime?: string,
+    numberOfPayment?: string,
+  },
+  readRefundAccount?: boolean,
+  reference?: string,
+  type: string,
+};
+
 const paymentAuth = async (request: Request): Promise<Response> => {
   if (request.method !== 'POST') {
     return new Response("Method Not Allowed", {
@@ -13,7 +98,7 @@ const paymentAuth = async (request: Request): Promise<Response> => {
     applicationUserId: body.applicationUserId,
     institutionId: body.institutionId,
     callback: 'https://display-parameters.com/',
-    paymentRequest: body.paymentRequest,
+    paymentRequest: body.paymentRequest as paymentRequest,
   };
 
   const data = await authCall('https://api.yapily.com/payment-auth-requests', reqBody, 'POST');
