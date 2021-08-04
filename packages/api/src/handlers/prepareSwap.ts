4108a0bad8712645647841d48a07bfa7ed07a739
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2021-08-04 15:17:44 +0100

    API: create swap details type

--- packages/api/src/handlers/prepareSwap.ts
+++ packages/api/src/handlers/prepareSwap.ts
@@ -1,3 +1,10 @@
+type SwapDetails = {
+  buyer: string,
+  seller: string,
+  currency: string,
+  token: string,
+  exchangeRate: string,
+}
 
 
 export const prepareSwap = async (request: Request): Promise<Response> => {
@@ -6,10 +13,14 @@ export const prepareSwap = async (request: Request): Promise<Response> => {
       status: 405
     });
   }
-  
-  console.log('TODO: check signature against swap details');
-  console.log('TODO: ')
   const body = await request.json();
+
+  const swapDetails: SwapDetails = body.swapDetails;
+
+  console.log('TODO: check signature against swap details');
+  console.log('TODO: fetch seller consent token');
+  console.log('TODO: create payment autorization');
+  console.log('TODO: create oracle signature ');
 
   return new Response('result',
     {
