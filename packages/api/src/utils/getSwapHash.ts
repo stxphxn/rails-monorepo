5b28a53cc275453916c69d543bacac4d58c7e422
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2021-08-08 20:07:10 +0100

    API: update getSwapHash

--- packages/api/src/utils/getSwapHash.ts
+++ packages/api/src/utils/getSwapHash.ts
@@ -4,6 +4,6 @@ import { SwapInfo } from "../types";
 export const getSwapHash = (swapInfo: SwapInfo): string => {
   const {buyer, seller, oracle, token, amount, expiry } = swapInfo;
   return utils.keccak256(utils.defaultAbiCoder.encode(
-    ['string', 'string', 'string', 'string', 'int256', 'int256'],
+    ['string', 'string', 'string', 'string', 'uint256', 'uint256'],
     [buyer, seller, oracle, token, amount, expiry]));
 };
