49c96ab8271b23b2b530eb0f6d766d2be9485f56
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2021-08-31 15:02:00 +0100

    API: fetch consent token in fulfill handler

--- packages/api/src/handlers/fulfillSwap.ts
+++ packages/api/src/handlers/fulfillSwap.ts
@@ -1,6 +1,9 @@
+import { fetchConsentToken } from "../helpers/fetchConsentToken";
+import { SwapInfo } from "../types";
 
-export type fulfillSwapRequestBody = {
-  
+export type FulfillSwapRequestBody = {
+  swapInfo: SwapInfo,
+  sellerInstitution: string,
 }
 
 export const fulfillSwap = async (request: Request): Promise<Response> => {
@@ -9,7 +12,15 @@ export const fulfillSwap = async (request: Request): Promise<Response> => {
       status: 405
     });
   }
+  const body: FulfillSwapRequestBody = await request.json();
+  const { swapInfo } = body;
 
+  // TODO: fetch seller consent token
+  const { consentToken } = await fetchConsentToken(swapInfo.seller, body.sellerInstitution);
+  
+  // TODO: check for payment transaction
+
+  // TODO: create release signature
 
   return new Response('response',
   {
