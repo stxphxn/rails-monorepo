511ce689031370037044fe476e54f43279fc1d97
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2021-10-20 22:03:40 +0100

    API: create new signature helper

---
+++ packages/api/src/helpers/signatures.ts
@@ -0,0 +1,54 @@
+import { BigNumber, ContractReceipt, Signer, Wallet } from 'ethers';
+import { defaultAbiCoder, solidityKeccak256, arrayify, splitSignature, Bytes } from 'ethers/lib/utils';
+
+export const tidy = (str: string): string => `${str.replace(/\n/g, "").replace(/ +/g, " ")}`;
+
+export const SignedDataEncoding = tidy(`tuple(
+  string functionIdentifier,
+  bytes32 swapHash
+)`);
+
+
+export const getSwapData = async (receipt: ContractReceipt, eventName: string) => {
+  const idx = receipt.events?.findIndex((e) => e.event === eventName) ?? -1;
+  const decoded = receipt.events![idx].decode!(receipt.events![idx].data, receipt.events![idx].topics);
+  return {
+    swapHash: decoded[0],
+    swapData: {
+      buyer: decoded[1][0],
+      seller: decoded[1][1],
+      oracle: decoded[1][2],
+      assetId: decoded[1][3],
+      amount: decoded[1][4],
+      swapId: decoded[1][5],
+      currencyHash: decoded[1][6],
+      prepareBlockNumber: decoded[1][7],
+      expiry: decoded[1][8],
+   }
+}
+};
+
+
+
+export const encodeSignatureData = (type: string, swapHash: Bytes): string => {
+  return defaultAbiCoder.encode(
+    [SignedDataEncoding],
+    [{ functionIdentifier: type, swapHash: swapHash}],
+  );
+};
+
+const sanitizeSignature = (sig: string): string => {
+  if (sig.endsWith("1c") || sig.endsWith("1b")) {
+    return sig;
+  }
+
+  // Must be sanitized
+  const { v } = splitSignature(sig);
+  const hex = BigNumber.from(v).toHexString();
+  return sig.slice(0, sig.length - 2) + hex.slice(2);
+};
+
+const sign = async (hash: string, signer: Wallet | Signer): Promise<string> => {
+  const msg = arrayify(hash);
+  return sanitizeSignature(await signer.signMessage(msg));
+};
