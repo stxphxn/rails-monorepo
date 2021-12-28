5c7a0b08ce249db4058c92eae2463d6547ed91fc
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2021-12-28 18:57:52 +0000

    API: update escrow helper

--- packages/api/src/helpers/escrow.ts
+++ packages/api/src/helpers/escrow.ts
@@ -1,6 +1,7 @@
 import { RailsEscrow__factory } from '@rails/contracts/src/types';
-import { ethers } from 'ethers'
+import { getDefaultProvider, Wallet } from 'ethers'
 
-const provider = ethers.getDefaultProvider('http://localhost:8545');
-export const wallet = ethers.Wallet.fromMnemonic(MNEMONIC).connect(provider);
+const provider = getDefaultProvider('http://127.0.0.1:8545/');
+export const wallet = Wallet.fromMnemonic(MNEMONIC).connect(provider);
 export const escrow = RailsEscrow__factory.connect(RAILS_ESCROW_ADDRESS, wallet);
+
