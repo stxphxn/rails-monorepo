fbdd7957ea7815f2c323d66d949a80084c7a67f4
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2022-04-24 18:55:07 +0100

    delete app package

--- packages/app/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './main.css';
-import './registerServiceWorker';
-import router from './router';
-import { store, key } from './store';
-
-createApp(App).use(store, key).use(router).mount('#app');
